c8a22db0cdf22bbac3e9d7080cbb038e6fe0ad21
Thomas Urbanski <tjurbanski@comcast.net>
2024-09-11 19:03:57 -0400

    Added a block

---
+++ src/main/java/net/ttttoooo/thelunaris/TheLunaris.java
@@ -0,0 +1,87 @@
+package net.ttttoooo.thelunaris;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.ttttoooo.thelunaris.block.ModBlocks;
+import net.ttttoooo.thelunaris.item.ModItems;
+
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TheLunaris.MODID)
+public class TheLunaris
+{
+    public static final String MODID = "thelunaris";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    
+    public TheLunaris()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+        
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM The Lunaris");
+        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+    	if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+    		event.accept(ModItems.MOONDIAL);
+    		event.accept(ModItems.MOONSTEEL);
+    		event.accept(ModItems.EMOONSTEEL);
+    		event.accept(ModItems.LUNARITE);
+    	}
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
---
+++ src/main/java/net/ttttoooo/thelunaris/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.ttttoooo.thelunaris.block;
+
+import net.minecraft.world.item.Item;
+
+import java.util.function.Supplier;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.ttttoooo.thelunaris.TheLunaris;
+import net.ttttoooo.thelunaris.item.ModItems;
+
+public class ModBlocks {
+	public static final DeferredRegister<Block> BLOCKS =
+			DeferredRegister.create(ForgeRegistries.BLOCKS, TheLunaris.MODID);
+	
+	public static final RegistryObject<Block> MOONDIALBLOCK = BLOCKS.register("moondialblock", 
+			() -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.ANCIENT_DEBRIS)));
+	
+	public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
+		RegistryObject<T> toReturn = BLOCKS.register(name, block);
+		registerBlockItem(name, toReturn);
+		return toReturn;
+	}
+	
+	public static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block){
+		return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+	}
+	
+	public static void register(IEventBus eventBus) {
+		BLOCKS.register(eventBus);
+	}
+}
