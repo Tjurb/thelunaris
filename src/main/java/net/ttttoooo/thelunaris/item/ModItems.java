009b56e1ccdca5a7451c8aacfdac20438b105811
Thomas Urbanski <tjurbanski@comcast.net>
2024-09-30 17:35:55 -0400

    created textures for new items, saplings, leaves, and woods

--- src/main/java/net/ttttoooo/thelunaris/item/ModItems.java
+++ src/main/java/net/ttttoooo/thelunaris/item/ModItems.java
@@ -13,12 +13,17 @@ public class ModItems {
 	
 	public static final RegistryObject<Item> MOONDIAL = ITEMS.register("moondial", 
 			() -> new Item(new Item.Properties()));
+	
+	//materials
 	public static final RegistryObject<Item> MOONSTEEL = ITEMS.register("moonsteel", 
 			() -> new Item(new Item.Properties()));
 	public static final RegistryObject<Item> EMOONSTEEL = ITEMS.register("emoonsteel", 
 			() -> new Item(new Item.Properties()));
 	public static final RegistryObject<Item> LUNARITE = ITEMS.register("lunarite", 
 			() -> new Item(new Item.Properties()));
+	
+	//tools
+	
 	
 	public static void register(IEventBus eventBus) {
 		ITEMS.register(eventBus);
